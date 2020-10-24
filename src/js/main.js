4e75bcbbbc20043aad6657aa94a99ab95f4cd0ed
PatriciaPoveda <patriciapovedaboto@gmail.com>
2020-10-24 13:35:26 +0200

    add search and favorites js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('>> Ready :)');
+// const btnSearch = document.querySelector('.js-btnSearch');
+// let series = [];
+// function getSerie(){
+//   const query = document.querySelector('.js-inputSearch').value;
+//   fetch(`http://api.tvmaze.com/search/shows?q=${query}`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data.show);
+//     })
+// };
+// btnSearch.addEventListener('click', getSerie);
+
